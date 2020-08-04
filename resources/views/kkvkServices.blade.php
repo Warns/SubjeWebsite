0e9fd3590e5c422d129a4182f21877bf3bc9c8b7
SanemG <30922736+SanemG@users.noreply.github.com>
2020-08-04 16:57:39 +0300

    Create kkvkServices.blade.php

---
+++ resources/views/kkvkServices.blade.php
@@ -0,0 +1,25 @@
+@section('pageTitle', 'KVKK Uyumluluk Çözümleri')
+@include('master/headerInfo')
+
+<!-- banner-bottom -->
+<section class="banner-bottom py-5" id="exp">
+    <div class="container">
+        <div class="row mid-grids">
+            <div class="col-md-12 content-left-bottom entry-w3layouts-info text-left">
+                <h5 class="mt-1">KVKK UYUMLULUK ÇÖZÜMLERİ</h5>
+                <p class="mt-2 text-left">Subje Bilişim Kişisel Verilerin Korunması Kanunu uyumluluk hizmetleri için ihtiyaç duyulan tüm yönetim süreçlerinde çözüm ortağınızdır.</p>
+                <p class="mt-2 text-left">Bilindiği üzere, 6698 sayılı Kişisel Verilerin Korunması Kanununun (Kanun) 12’nci maddesinin (1) numaralı fıkrasında veri sorumlusunun;</p>
+                <p class="mt-2 text-left">Kişisel verilerin hukuka aykırı olarak işlenmesini önlemek,</p>
+                <p class="mt-2 text-left">Kişisel verilere hukuka aykırı olarak erişilmesini önlemek,</p>
+                <p class="mt-2 text-left">Kişisel verilerin muhafazasını sağlamak</p>
+                <p class="mt-2 text-left">amacıyla uygun güvenlik düzeyini temin etmeye yönelik gerekli her türlü teknik ve idari tedbirleri almak zorunda olduğu belirtilmiştir. Talebiniz doğrultusunda KVKK uzmanları ve KVKK da uzmanlaşmış hukukçulardan hizmet alınarak durum tespiti çalışması yapılır.  Şirketinizin KVKK yönetim gereksinimlerini tek noktaya toplar. </p>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- //banner-bottom -->
+
+
+@include('master/footer')
+
