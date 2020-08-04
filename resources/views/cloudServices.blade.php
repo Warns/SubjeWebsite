4cae8779d8b845d0ba25b0d1c278b3b117fb5cab
SanemG <30922736+SanemG@users.noreply.github.com>
2020-08-04 16:56:04 +0300

    Create cloudServices.blade.php

---
+++ resources/views/cloudServices.blade.php
@@ -0,0 +1,21 @@
+@section('pageTitle', 'Bulut Çözümleri')
+@include('master/headerInfo')
+
+<!-- banner-bottom -->
+<section class="banner-bottom py-5" id="exp">
+    <div class="container">
+        <div class="row mid-grids">
+            <div class="col-md-12 content-left-bottom entry-w3layouts-info text-left">
+                <h5 class="mt-1">BULUT ÇÖZÜMLERİ</h5>
+                <p class="mt-2 text-left">Bugünün ve geleceğin teknolojisi  Bulut Bilgi İşlem, iş dünyasının ve IT' nin kritik bir bileşenidir. Sizin için en doğru bulut çözüm üreticileri ile  şirketinizi daha ileriye taşımak için bizimle iletişime geçiniz.</p>
+
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- //banner-bottom -->
+
+
+@include('master/footer')
+
