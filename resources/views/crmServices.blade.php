0e6d9d234d383330a8ae31d5fd5699b436d74e17
SanemG <30922736+SanemG@users.noreply.github.com>
2020-08-04 16:56:14 +0300

    Create crmServices.blade.php

---
+++ resources/views/crmServices.blade.php
@@ -0,0 +1,20 @@
+@section('pageTitle', 'CRM Çözümleri')
+@include('master/headerInfo')
+
+<!-- banner-bottom -->
+<section class="banner-bottom py-5" id="exp">
+    <div class="container">
+        <div class="row mid-grids">
+            <div class="col-md-12 content-left-bottom entry-w3layouts-info text-left">
+                <h5 class="mt-1">CRM ÇÖZÜMLERİ</h5>
+                <p class="mt-2 text-left">CRM Müşteri İlişkileri Yönetimi, kurumların müşterileri ile ilişkilerine dair hemen hemen tüm sektörlerde pazarlama ve satış alanında çözümler sunmaktadır. Bu noktada şirketlerinize iş süreçlerinde hızlı ve etkin olarak katkı sağlamaktadır.</p>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- //banner-bottom -->
+
+
+@include('master/footer')
+
