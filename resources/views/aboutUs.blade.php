a895e51be7149aab1b7345946471ef7c7d0c81e3
SanemG <30922736+SanemG@users.noreply.github.com>
2020-08-04 16:55:55 +0300

    Create aboutUs.blade.php

---
+++ resources/views/aboutUs.blade.php
@@ -0,0 +1,25 @@
+@section('pageTitle', 'Hakkımızda')
+@include('master/headerInfo')
+
+<!-- banner-bottom -->
+<section class="banner-bottom py-5" id="exp">
+    <div class="container py-md-4">
+        <h3 class="tittle-w3layouts two text-center">Hakkımızda</h3>
+        <div class="row mid-grids mt-5">
+            <div class="col-md-5 content-w3pvt-img">
+                <img src="images/n1.jpg" alt="" class="img-fluid">
+            </div>
+            <div class="col-md-7 content-left-bottom entry-w3layouts-info text-left mt-3">
+                <h5 class="mt-1">Misyonumuz</h5>
+                <p class="mt-2 text-left">Subje, kurumlar için  sürdürülebilir proje konseptleri oluşturur. Çözüm ortaklarıyla, projenin gerekliliklerini belirleyip,kurumlara en uygun ve yenilikçi çözümler sunar. Subje  uzun soluklu iş birliği kurabileceğiniz kurumsal iş ortağınızdır. Yenilikçi bakış açısıyla kurumlara verimliliği arttırıcı karlı çözümler sunan teknoloji ve danışmanlık firmasıdır.</p>
+
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- //banner-bottom -->
+
+
+@include('master/footer')
+
