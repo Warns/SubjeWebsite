ba6124176617b9cae7c407dc0f0c30845837f046
SanemG <30922736+SanemG@users.noreply.github.com>
2020-08-04 16:56:08 +0300

    Create consultServices.blade.php

---
+++ resources/views/consultServices.blade.php
@@ -0,0 +1,21 @@
+@section('pageTitle', 'Danışmanlık Hizmetleri')
+@include('master/headerInfo')
+
+<!-- banner-bottom -->
+<section class="banner-bottom py-5" id="exp">
+    <div class="container">
+        <div class="row mid-grids">
+            <div class="col-md-12 content-left-bottom entry-w3layouts-info text-left">
+                <h5 class="mt-1">DANIŞMANLIK HİZMETLERİ</h5>
+                <p class="mt-2 text-left">Subje, kurumlar için  sürdürülebilir proje konseptleri oluşturur. Çözüm ortaklarıyla, projenin gerekliliklerini belirleyip,kurumlara en uygun ve yenilikçi çözümler sunar. Subje  uzun soluklu iş birliği kurabileceğiniz kurumsal iş ortağınızdır. Yenilikçi bakış açısıyla kurumlara verimliliği arttırıcı karlı çözümler sunan teknoloji ve danışmanlık firmasıdır.</p>
+
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- //banner-bottom -->
+
+
+@include('master/footer')
+
