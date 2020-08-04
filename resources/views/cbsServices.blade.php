bc20bffd1f4e5c43ec993a9b0d6b9f6d9f5ebb5d
SanemG <30922736+SanemG@users.noreply.github.com>
2020-08-04 16:56:01 +0300

    Create cbsServices.blade.php

---
+++ resources/views/cbsServices.blade.php
@@ -0,0 +1,21 @@
+@section('pageTitle', 'Coğrafi Bilgi Sistemi Danışmanlık Hizmetleri')
+@include('master/headerInfo')
+
+<!-- banner-bottom -->
+<section class="banner-bottom py-5" id="exp">
+    <div class="container">
+        <div class="row mid-grids">
+            <div class="col-md-12 content-left-bottom entry-w3layouts-info text-left">
+                <h5 class="mt-1">COĞRAFİ BİLGİ SİSTEMİ DANIŞMANLIK HİZMETLERİ</h5>
+                <p class="mt-2 text-left">Kurumsal CBS ihtiyaçlarınızı karşılamak, hedefleriniz doğrultusunda profesyonel danışmanlık hizmetleri sunmaktayız. Analiz, yol haritası ve kurulum, entegrasyon süreçlerinde en büyük destekçiniziz.</p>
+
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- //banner-bottom -->
+
+
+@include('master/footer')
+
