e875ea76c1db1db7416edb9a4c71383f38716284
SanemG <30922736+SanemG@users.noreply.github.com>
2020-08-04 16:57:45 +0300

    Create dataServices.blade.php

---
+++ resources/views/dataServices.blade.php
@@ -0,0 +1,20 @@
+@section('pageTitle', 'Veri Envanter Yönetimi Çözümleri')
+@include('master/headerInfo')
+
+<!-- banner-bottom -->
+<section class="banner-bottom py-5" id="exp">
+    <div class="container">
+        <div class="row mid-grids">
+            <div class="col-md-12 content-left-bottom entry-w3layouts-info text-left">
+                <h5 class="mt-1">VERİ ENVANTER YÖNETİMİ ÇÖZÜMLERİ</h5>
+                <p class="mt-2 text-left">Veri Envanter Yönetimine yönelik kurumsal ihtiyaçlarınıza, hedefleriniz doğrultusunda iş süreci ve proje danışmanlığı hizmeti sunmaktayız. Hedefimiz şirketinizin çıkarları doğrultusunda en doğru çözüm ortaklarıyla buluşturarak verimlilik ve karlılığınızı en üst seviyeye çıkarmaktır. </p>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- //banner-bottom -->
+
+
+@include('master/footer')
+
