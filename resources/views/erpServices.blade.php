185ae1ae56fabe1e63d71d8ee48e68798de12f7e
SanemG <30922736+SanemG@users.noreply.github.com>
2020-08-04 16:57:42 +0300

    Create erpServices.blade.php

---
+++ resources/views/erpServices.blade.php
@@ -0,0 +1,20 @@
+@section('pageTitle', 'ERP Çözümleri')
+@include('master/headerInfo')
+
+<!-- banner-bottom -->
+<section class="banner-bottom py-5" id="exp">
+    <div class="container">
+        <div class="row mid-grids">
+            <div class="col-md-12 content-left-bottom entry-w3layouts-info text-left">
+                <h5 class="mt-1">ERP ÇÖZÜMLERİ</h5>
+                <p class="mt-2 text-left">Şirketiniz ile yapılacak görüşmeler sonrası sizin için en doğru ERP (Kurumsal Kaynak Planlama) yol haritası çıkarılır süreçleri titizlikle takip edilir. Şirketinizin mal ve hizmet üretimi için gereken kaynakların verimli bir şekilde kullanılmasını sağlamak için çözüm ortağınızız.</p>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- //banner-bottom -->
+
+
+@include('master/footer')
+
