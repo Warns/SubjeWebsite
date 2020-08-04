3df1430aed4226fa52120986478cdfd74dc3d2b5
SanemG <30922736+SanemG@users.noreply.github.com>
2020-08-04 16:57:36 +0300

    Create landing.blade.php

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,126 @@
+@section('pageTitle', 'Anasayfa')
+@include('master/header')
+<!-- banner -->
+<div class="banner-content-w3pvt">
+    <div class="banner-w3layouts-info text-center">
+        <h3>İşbirliği Entegrasyon Danışmanlık</h3>
+        <p class="sub-test pr-lg-5">Biz, şirketinizin ihtiyaçları doğrultusunda uzman danışman kadromuzla sizin için en uygun IT çözümlerini belirliyoruz.</p>
+    </div>
+</div>
+
+<!-- //banner -->
+</div>
+<!-- //main -->
+<!-- /entry -->
+<div class="entry-w3pvt-main py-5 landing-margin">
+    <div class="container px-lg-5 py-md-3">
+        <div class="entry-w3layouts-info">
+            <h4>Subje Bilişim</h4>
+            <p>Sektörünüze uygun danışmanlarla analiz ve planlama yaparak ihtiyaçlarınızı belirleriz. Yeni nesil teknolojilerle ihtiyaçlarınızı karşılamak üzere anahtar teslim çözümler sunarız. Sorunsuz bir şekilde şirketinizin rekabet gücünü arttırırız.
+            </p>
+        </div>
+    </div>
+</div>
+<!-- //entry -->
+<!-- banner-bottom -->
+<section class="banner-bottom py-5" id="about">
+    <div class="container py-md-4">
+        <div class="row banner-grids">
+            <div class="col-lg-6 content-right-bottom text-left">
+                <img src="images/ab.jpg" alt="" class="img-fluid">
+            </div>
+            <div class="col-lg-6 content-left-bottom entry-w3layouts-info text-left pl-lg-4">
+                <h5 class="mt-1">Subje Bilişim.</h5>
+                <h4>Hakkımızda</h4>
+                <p class="mt-2 text-left">Subje, kurumlar için  sürdürülebilir proje konseptleri oluşturur. Çözüm ortaklarıyla, projenin gerekliliklerini belirleyip,kurumlara en uygun ve yenilikçi çözümler sunar. Subje  uzun soluklu iş birliği kurabileceğiniz kurumsal iş ortağınızdır. Yenilikçi bakış açısıyla kurumlara verimliliği arttırıcı karlı çözümler sunan teknoloji ve danışmanlık firmasıdır.</p>
+
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- //banner-bottom -->
+<!-- /team-content -->
+<section class="team-content py-5" id="team">
+    <div class="container py-md-4 text-center">
+        <h3 class="tittle-w3layouts two">Hizmetlerimiz</h3>
+        <div class="row team-grids text-center mt-md-5 mt-4">
+            <div class="col-lg-4 team-main">
+
+                <div class="team-info">
+
+                    <h4>Danışmanlık Hizmetleri</h4>
+                    <p class="mt-2 text-left">Şirketinizin ihtiyaçları doğrultusunda uzman danışman kadromuzla sizin için en uygun IT çözümleri belirlenir. Doğru analiz ile yol haritanız belirlendikten sonra ürün-hizmet yapınızın, birimlerinizin...</p>
+                </div>
+
+
+            </div>
+
+            <div class="col-lg-4 team-main">
+                <div class="team-info">
+
+                    <h4>Bulut Çözümleri</h4>
+                    <p>Bugünün ve geleceğin teknolojisi Bulut Bilgi İşlem, iş dünyasının ve IT' nin kritik bir bileşenidir. Sizin için en doğru bulut çözüm üreticileri ile şirketinizi daha ileriye taşımak için bizimle iletişime geçiniz.</p>
+                </div>
+
+
+
+            </div>
+
+            <div class="col-lg-4 team-main">
+
+                <div class="team-info">
+
+                    <h4>CRM Çözümleri</h4>
+                    <p>CRM Müşteri İlişkileri Yönetimi, kurumların müşterileri ile ilişkilerini hemen hemen tüm sektörlerde pazarlama ve satış alanında çözümler sunmaktadır. Bu noktada şirketlerinize iş süreçlerinde hızlı ve etkin olarak...</p>
+                </div>
+
+
+            </div>
+
+        </div>
+
+    </div>
+</section>
+<!-- //team-content -->
+<!-- banner-bottom -->
+<section class="banner-bottom py-5" id="exp">
+    <div class="container py-md-4">
+        <h3 class="tittle-w3layouts two text-center">Ne Yapıyoruz</h3>
+        <div class="row mid-grids mt-5">
+            <div class="col-md-5 content-w3pvt-img">
+                <img src="images/n1.jpg" alt="" class="img-fluid">
+            </div>
+            <div class="col-md-7 content-left-bottom entry-w3layouts-info text-left mt-3">
+                <h5 class="mt-1">ZAMANDAN TASARRUF ETTİRİRİZ</h5>
+                <p class="mt-2 text-left">İş temponuza göre eylem planlayıp taleplerinize sunduğumuz çözümlerle hızlı ve etkin hizmet sağlarız.</p>
+                <h5 class="mt-1 mt-md-4">TAKIM RUHU İLE ÇALIŞIRIZ</h5>
+                <p class="mt-2 text-left">Uzman personel ve yetkin danışmanlarımızla ekip çalışması yürüterek tutarlı ve ihtiyacınızı kapsayıcı çözümler üretiriz.</p>
+                <h5 class="mt-1 mt-md-4">BİR ADIM ÖTESİNİ DÜŞÜNÜRÜZ</h5>
+                <p class="mt-2 text-left">İhtiyaçları ve pazarı dikkate alarak kendimizi daima geliştirip sektörde öncü ve nitelikli projeler ortaya koyarız. Bu sizi rakiplerinizin önüne geçirir.</p>
+            </div>
+
+        </div>
+        <div class="row mid-grids my-lg-5 py-lg-5">
+
+            <div class="col-md-7 content-left-bottom entry-w3layouts-info text-left mt-3">
+                <h5 class="mt-1">KALİTELİ İŞ ÇIKARTIRIZ</h5>
+                <p class="mt-2 text-left">En ileri düzeyde çözümler ile istediğiniz kaliteli hizmeti sunarız. Böylece kalitenizden ödün vermezsiniz.</p>
+                <h5 class="mt-1 mt-md-4">PARANIZ DEĞER KAZANIR</h5>
+                <p class="mt-2 text-left">İhtiyaçlarınız ve bütçeniz çerçevesinde kaynaklarımızı gözden geçirip çözüm önerileri sunarız.</p>
+                <h5 class="mt-1 mt-md-4">GÜVEN TEMİN EDERİZ</h5>
+                <p class="mt-2 text-left">İş birliği ile kontrollü bir süreç sağlar gizliliğe dikkat ederiz ve riskleri öngörerek güvenle ilerlemenizi sağlarız.</p>
+
+            </div>
+            <div class="col-md-5 content-w3pvt-img">
+                <img src="images/n2.jpg" alt="" class="img-fluid">
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
+
+@include('master/footer')
+
