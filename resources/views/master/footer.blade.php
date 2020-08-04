c4985c993fde816cc08430928f19b049af884050
SanemG <30922736+SanemG@users.noreply.github.com>
2020-08-04 16:57:33 +0300

    Create footer.blade.php

---
+++ resources/views/master/footer.blade.php
@@ -0,0 +1,50 @@
+<!-- footer -->
+<footer class="footer-content text-center py-5">
+    <div class="container py-md-3">
+        <!-- logo -->
+        <h2 class="logo2 text-center">
+            <a href="index.html">
+                Subje Bilişim
+            </a>
+        </h2>
+        <!-- //logo -->
+        <!-- address -->
+        <div class="contact-left-footer mt-md-4">
+            <ul>
+                <li>
+                    <p>
+                        <span class="fa fa-map-marker mr-2"></span>İKİTELLİ OSB MAH. YTÜ İKİTELLİ TEKNOPARK SK. YTÜ TEKNOPARK NO: 1 İÇ KAPI NO: Z20 BAŞAKŞEHİR/ İSTANBUL
+                    </p>
+                </li>
+                <li class="mx-4">
+                    <p>
+                        <span class="fa fa-phone mr-2"></span>0212 671 91 38
+                    </p>
+                </li>
+                <li>
+                    <p class="text-wh">
+                        <span class="fa fa-envelope-open mr-2"></span>
+                        <a href="mailto:info@example.com">info@subjebilisim.com</a>
+                    </p>
+                </li>
+            </ul>
+        </div>
+        <!-- //address -->
+        <!-- copyright -->
+        <div class="w3layouts-copy text-center">
+            <p class="text-da">Copyright © 2019, Subje Bilişim, Tüm Hakları Saklıdır.
+            </p>
+        </div>
+        <!-- //copyright -->
+        <!-- move top icon -->
+        <div class="move-top text-center mt-3">
+            <a href="#home" class="move-top"><span class="fa fa-angle-double-up" aria-hidden="true"></span></a>
+        </div>
+        <!-- //move top icon -->
+    </div>
+</footer>
+<!-- //footer -->
+
+</body>
+
+</html>
