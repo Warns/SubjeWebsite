c4238ce30f793d252b97bc91904aebafee731c50
SanemG <30922736+SanemG@users.noreply.github.com>
2020-08-04 16:57:23 +0300

    Create softwareDev.blade.php

---
+++ resources/views/softwareDev.blade.php
@@ -0,0 +1,20 @@
+@section('pageTitle', 'Özel Yazılım ve Yazılım Geliştirme Çözümleri')
+@include('master/headerInfo')
+
+<!-- banner-bottom -->
+<section class="banner-bottom py-5" id="exp">
+    <div class="container">
+        <div class="row mid-grids">
+            <div class="col-md-12 content-left-bottom entry-w3layouts-info text-left">
+                <h5 class="mt-1">ÖZEL YAZILIM VE YAZILIM GELİŞTİRME ÇÖZÜMLERİ</h5>
+                <p class="mt-2 text-left">Farklı sektörlerdeki kurumlara ihtiyaçları ve iş mimarileri doğrultusunda özel yazılım geliştirme yaparak yenilikçi ve güvenilir hizmet vermekteyiz. Böylelikle sektöründe kurumları bir adım öne geçirmekteyiz.</p>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- //banner-bottom -->
+
+
+@include('master/footer')
+
