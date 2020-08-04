9067b6cccd6635dc8751358cde3d43e7ca6ea3bd
SanemG <30922736+SanemG@users.noreply.github.com>
2020-08-04 16:55:58 +0300

    Create blockChain.blade.php

---
+++ resources/views/blockChain.blade.php
@@ -0,0 +1,21 @@
+@section('pageTitle', 'Blockchain Çözümleri')
+@include('master/headerInfo')
+
+<!-- banner-bottom -->
+<section class="banner-bottom py-5" id="exp">
+    <div class="container">
+        <div class="row mid-grids">
+            <div class="col-md-12 content-left-bottom entry-w3layouts-info text-left">
+                <h5 class="mt-1">BLOCKCHAIN ÇÖZÜMLERİ</h5>
+                <p class="mt-2 text-left">Sisteminizi blockchain teknolojisiyle koruma altına alarak siber güvenlik önlemlerinizi üst seviyelere çıkartıyoruz.</p>
+
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- //banner-bottom -->
+
+
+@include('master/footer')
+
