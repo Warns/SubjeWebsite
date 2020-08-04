783c0d590bda1bb6984284444d7c1a4b642fbfd7
SanemG <30922736+SanemG@users.noreply.github.com>
2020-08-04 16:56:18 +0300

    Create cyberSecurity.blade.php

---
+++ resources/views/cyberSecurity.blade.php
@@ -0,0 +1,20 @@
+@section('pageTitle', 'Siber Güvenlik Çözümleri')
+@include('master/headerInfo')
+
+<!-- banner-bottom -->
+<section class="banner-bottom py-5" id="exp">
+    <div class="container">
+        <div class="row mid-grids">
+            <div class="col-md-12 content-left-bottom entry-w3layouts-info text-left">
+                <h5 class="mt-1">SİBER GÜVENLİK ÇÖZÜMLERİ</h5>
+                <p class="mt-2 text-left">Günümüzde bilgi teknolojilerini aktif bir şekilde kullanan kurumlar için en büyük tehlikelerden biri de siber güvenlik tehditidir. Kurumunuz  için entegre risk yönetimi, etkili politika ve kontrollerin geliştirilebilmesi için güvenlik sürecinin en doğru şekilde yürütülmesi gerekir. Size özel sunulan çözümler ile doğabilecek tehditlere karşı güvendesiniz.</p>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- //banner-bottom -->
+
+
+@include('master/footer')
+
