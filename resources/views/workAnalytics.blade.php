2ab131a51dde9d4924dd188994079a7b2fba3d67
SanemG <30922736+SanemG@users.noreply.github.com>
2020-08-04 16:57:18 +0300

    Create workAnalytics.blade.php

---
+++ resources/views/workAnalytics.blade.php
@@ -0,0 +1,20 @@
+@section('pageTitle', 'İş Analitiği ve İş Zekası Çözümleri')
+@include('master/headerInfo')
+
+<!-- banner-bottom -->
+<section class="banner-bottom py-5" id="exp">
+    <div class="container">
+        <div class="row mid-grids">
+            <div class="col-md-12 content-left-bottom entry-w3layouts-info text-left">
+                <h5 class="mt-1">İŞ ANALİTİĞİ VE İŞ ZEKASI ÇÖZÜMLERİ</h5>
+                <p class="mt-2 text-left">Firmaların hedef ve stratejilerine uygun veri analizi ve veri görselleştirmeleri yapan yeni nesil ve hızlı iş analitiği ve iş zekası çözümleri sunuyoruz.</p>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- //banner-bottom -->
+
+
+@include('master/footer')
+
